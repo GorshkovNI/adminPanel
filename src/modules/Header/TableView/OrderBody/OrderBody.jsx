72537e1c1a937871d6f0cdc93826fb36138f0b6b
Nikita Gorshkov <98645018+GorshkovNI@users.noreply.github.com>
2022-11-12 17:31:10 +0500

    правки

--- src/modules/Header/TableView/OrderBody/OrderBody.jsx
+++ src/modules/Header/TableView/OrderBody/OrderBody.jsx
@@ -2,28 +2,13 @@ import React from 'react';
 import styles from './OrderBody.module.css';
 import cn from 'classnames';
 import { TableBody } from '../../../../shared/Table/TableBody/TableBody';
-import { TableRow } from '../../../../shared/Table/TableBody/TableRow/TableRow';
-import { TableCell } from '../../../../shared/Table/TableBody/TableCell/TableCell';
-import { StatusCell } from '../../../../shared/Table/TableBody/StatusCell/StatusCell';
-import { CheckBox } from '../../../../shared/Checkbox/Checkbox';
+import { OrderRow } from './OrderRow/OrderRow';
 
 export const OrderBody = ({ className, date }) => {
   return (
     <TableBody className={cn(styles._, className)}>
-      {date.slice(0, 10).map((item) => {
-        return (
-          <TableRow key={item.sum + ' ' + item.orderNumber}>
-            <TableCell className={styles.cell}>
-              <CheckBox />
-            </TableCell>
-            <TableCell className={styles.cell}>{item.orderNumber}</TableCell>
-            <TableCell className={styles.cell}>{item.date}</TableCell>
-            <StatusCell classNames={styles.cell} status={item.status} />
-            <TableCell className={styles.cell}>{item.amount}</TableCell>
-            <TableCell className={styles.cell}>{item.sum}</TableCell>
-            <TableCell className={styles.cell}>{item.customer}</TableCell>
-          </TableRow>
-        );
+      {date.slice(0, 100).map((item, index) => {
+        return <OrderRow key={index} item={item} />;
       })}
     </TableBody>
   );
---
+++ src/modules/Header/TableView/OrderBody/OrderRow/OrderRow.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './OrderRow.module.css';
+import { CheckBox } from '../../../../../shared/Checkbox/Checkbox';
+import { StatusCell } from '../../../../../shared/Table/TableBody/StatusCell/StatusCell';
+import { TableCell } from '../../../../../shared/Table/TableBody/TableCell/TableCell';
+import { TableRow } from '../../../../../shared/Table/TableBody/TableRow/TableRow';
+
+export const OrderRow = ({ item }) => {
+  return (
+    <TableRow key={item.sum + ' ' + item.orderNumber}>
+      <TableCell className={styles.cell}>
+        <CheckBox />
+      </TableCell>
+      <TableCell className={styles.cell}>{item.orderNumber}</TableCell>
+      <TableCell className={styles.cell}>{item.date}</TableCell>
+      <StatusCell classNames={styles.cell} status={item.status} />
+      <TableCell className={styles.cell}>{item.amount}</TableCell>
+      <TableCell className={styles.cell}>{item.sum}</TableCell>
+      <TableCell className={styles.cell}>{item.customer}</TableCell>
+    </TableRow>
+  );
+};
